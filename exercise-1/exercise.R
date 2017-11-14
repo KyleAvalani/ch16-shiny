ca8b502db9c90c9ecfeafe5094783c294a152660
Alina Burke <alinamburke@gmail.com>
2017-11-14 14:00:09 -0800

    Complete exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,12 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/info201/ch16-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
+source('scripts/BuildScatter.r')
+data <- data.frame(iris)
 
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+ScatterplotBuilding(data, data$Sepal.Length, data$Sepal.Width, "blue", title="Iris Sepal Length vs Width", xlab="Length", ylab="Width")
